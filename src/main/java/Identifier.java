2faa5354a26258f3e50ce33700d96e3eb486ca88
wesleybudding <wesleybudding@gmail.com>
2017-09-20 15:24:11 +0200

    Identifier & import biginteger / hashmap

---
+++ src/main/java/Identifier.java
@@ -0,0 +1,7 @@
+/**
+ * Created by wesleybudding on 20/09/2017.
+ */
+public class Identifier {
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,23 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+
+// Create a scanner on System.in
+
+// While there is input, read line and parse it.
+
+// Lucas: maakt Set <E> ADT
+// Lucas: maakt punt 3 list
+// Wesley: maakt API big integer & hasmap
+// Wesley: maakt identifier ADT
+
+
 public class Main {
 
     private void start() {
-        // Create a scanner on System.in
-        
-        // While there is input, read line and parse it.
 
-        // Lucas: maakt Set <E> ADT
-        // Lucas: maakt punt 3 list
-        // Wesley: maakt API big integer & hasmap
-        // Wesley: maakt identifier ADT
     }
+
+
 
     public static void main(String[] argv) {
         new Main().start();

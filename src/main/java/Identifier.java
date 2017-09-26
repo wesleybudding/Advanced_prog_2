f7c9c3595fb3fc6faf692ceb79ff157d12d8ff21
wesleybudding <wesleybudding@gmail.com>
2017-09-26 21:09:26 +0200

    Identifier build

--- src/main/java/Identifier.java
+++ src/main/java/Identifier.java
@@ -3,5 +3,49 @@
  */
 public class Identifier {
 
+    //char[] nonZeroNumber =  {"1","2","3","4","5","6","7","8","9" };
+    char[] nonZeroNumber = "123456789".toCharArray();
+    char[] letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    //char[] letter =  { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    //                    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" };
+
+
+    boolean number(char x){
+        for(int i = 0; i<9; i++){
+            Character check = new Character(x);
+            if(check.equals(nonZeroNumber[i])){
+                return true;
+                //System.out.println("true");
+            }
+        }
+            return false;
+    }
+
+    boolean zero(char x){
+            if(x==0){
+                return true;
+            }
+
+        return false;
+    }
+
+    boolean letter(char x){
+        Character check = new Character(x);
+        for(int i = 0; i<51; i++){
+            if(check.equals(letter[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean space(char x){
+        Character check = new Character(x);
+        if(check.equals(" ")){
+            return true;
+        }
+        return false;
+    }
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.Scanner;
 
 // Create a scanner on System.in
 
@@ -9,11 +10,36 @@ import java.util.HashMap;
 // Lucas: maakt punt 3 list
 // Wesley: maakt API big integer & hasmap
 // Wesley: maakt identifier ADT
+// https://www.javaworld.com/article/2076974/learn-java/build-an-interpreter-in-java----implement-the-execution-engine.html
+// https://ruslanspivak.com/lsbasi-part1/
+// https://en.wikipedia.org/wiki/Extended_Backus–Naur_form
 
 
 public class Main {
 
     private void start() {
+        Scanner data = new Scanner(System.in);
+        Identifier iden = new Identifier();
+
+        System.out.println("Test input:");
+
+        String file = data.nextLine();
+        for(int i = 0 ; i<file.length();i++){
+            System.out.println("testing: " + file.charAt(i));
+            if(iden.number(file.charAt(i))){
+                System.out.println("number");
+            }else if(iden.zero(file.charAt(i))){
+                System.out.println("zero");
+            }else if(iden.letter(file.charAt(i))){
+                System.out.println("letter");
+            }else if(iden.space(file.charAt(i))){
+                System.out.println("letter");
+            }else{
+                System.out.println("invallid ");
+            }
+
+        }
+
 
     }
 
